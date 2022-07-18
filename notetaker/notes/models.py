17ab1fb82555cffaf76efd076bc2f13d5fbc8555
Binyan Li <lbyxiafei@gmail.com>
2022-07-17 21:12:25 -0700

    Check point, 2022-07-17T21:12:25.

--- notetaker/notes/models.py
+++ notetaker/notes/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Bookmark(models.Model):
+    name = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200)
+    url = models.CharField(max_length=200)
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    tag = models.CharField(max_length=200)
+    content = models.TextField()
+    pub_date = models.DateTimeField('date published')
+
+    def __str__(self) -> str:
+        return self.title
