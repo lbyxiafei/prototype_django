70d57b9bab6666e538f0ed03b87f149a74aad8c5
Binyan Li <binyanli@microsoft.com>
2022-07-26 00:06:57 -0700

    Onboard post.

--- notetaker/notes/api/urls.py
+++ notetaker/notes/api/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 
-from .views import BookmarkViewSet, TagViewSet 
+from .views import BookmarkViewSet,PostViewSet,TagViewSet 
 
 router = DefaultRouter()
 router.register("bookmarks", BookmarkViewSet, basename="bookmarks")
+router.register("posts", PostViewSet, basename="posts")
 router.register("tags", TagViewSet, basename="tags")
 
 urlpatterns = [
--- notetaker/notes/api/views.py
+++ notetaker/notes/api/views.py
@@ -2,8 +2,8 @@ from django.http import Http404
 from rest_framework import viewsets,status
 from rest_framework.response import Response
 
-from notes.models import Bookmark,Tag
-from .serializers import BookmarkSerializer, TagSerializer
+from notes.models import Bookmark,Post,Tag
+from .serializers import BookmarkSerializer,PostSerializer,TagSerializer
 
 class BookmarkViewSet(viewsets.ModelViewSet):
     serializer_class = BookmarkSerializer 
@@ -17,13 +17,10 @@ class BookmarkViewSet(viewsets.ModelViewSet):
         new_bookmark = Bookmark.objects.create(
             title=data["title"], url=data['url'])
         new_bookmark.save()
-
         for tag in data["tags"]:
             tag_obj = Tag.objects.get_or_create(name=tag["name"])
             new_bookmark.tags.add(tag_obj[0])
-
         serializer = BookmarkSerializer(new_bookmark)
-
         return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
@@ -34,6 +31,31 @@ class BookmarkViewSet(viewsets.ModelViewSet):
             pass
         return Response(status=status.HTTP_204_NO_CONTENT)
 
+class PostViewSet(viewsets.ModelViewSet):
+    serializer_class = PostSerializer 
+
+    def get_queryset(self):
+        posts = Post.objects.all()
+        return posts
+
+    def create(self, request, *args, **kwargs):
+        data = request.data
+        new_post = Post.objects.create(
+            title=data["title"], content=data['content'])
+        new_post.save()
+        for tag in data["tags"]:
+            tag_obj = Tag.objects.get_or_create(name=tag["name"])
+            new_post.tags.add(tag_obj[0])
+        serializer = PostSerializer(new_post)
+        return Response(serializer.data)
+
+    def destroy(self, request, *args, **kwargs):
+        try:
+            instance = self.get_object()
+            self.perform_destroy(instance)
+        except Http404:
+            pass
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 class TagViewSet(viewsets.ModelViewSet):
     serializer_class = TagSerializer
