6855350ddd772d770aebc37f809094489dff8ac7
Binyan Li <binyanli@microsoft.com>
2022-08-01 09:44:58 -0700

    Update with default postgres configs.

--- notetaker/notetaker/settings.py
+++ notetaker/notetaker/settings.py
@@ -87,10 +87,10 @@ WSGI_APPLICATION = 'notetaker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_NAME'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': 'db',
+        'NAME': os.environ.get('POSTGRES_NAME', 'notes'),
+        'USER': os.environ.get('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', 'admin'),
+        'HOST': os.environ.get('POSTGRES_HOST', 'db'),
         'PORT': 5432,
     }
 }
